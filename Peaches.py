92b00e09321b6c1ec548287234c0006451ac98f5
Lavender32 <77857798+Lavender32@users.noreply.github.com>
2024-01-29 01:46:09 +0000

    added in input

--- Peaches.py
+++ Peaches.py
@@ -1,39 +1,39 @@
-name = "Nic"
+name = input("please tell me your name: ")
 
-message = f"Hello {name}"
-
-print(message)
+message = f"Hello, {name}"
 
 print(message)
 
-print(message)
+# print(message)
 
-# print("horses")
+# print(message)
 
-# print("cows jumping over the moon")
+# # print("horses")
 
-# print("icecream")
+# # print("cows jumping over the moon")
 
-# print("cats, and hats")
+# # print("icecream")
 
-# print("it's raining cats and dogs")
+# # print("cats, and hats")
+
+# # print("it's raining cats and dogs")
       
-# print("houses on chicken feet")
+# # print("houses on chicken feet")
 
-# print("cats are better then birds")
+# # print("cats are better then birds")
 
-# print("flames and candle, wax ro") #the privious statement will still exicute
+# # print("flames and candle, wax ro") #the privious statement will still exicute
 
-# print is a function that tskes in a string and "prints" it to the console window
+# # print is a function that tskes in a string and "prints" it to the console window
 
-animal = "Wolves" # assigning the string value of "wolves" to to variable "animals".
+# animal = "Wolves" # assigning the string value of "wolves" to to variable "animals".
 
 
-animal2 = "sheep"
+# animal2 = "sheep"
 
-haukiu = f"{animal} love to eat {animal2}"
+# haukiu = f"{animal} love to eat {animal2}"
 
-print(haukiu)
+# print(haukiu)
 
 
 
