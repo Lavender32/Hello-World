95b97420cc4fd2fa4594393a4a6ea5bc05828c89
Lavender32 <77857798+Lavender32@users.noreply.github.com>
2024-01-29 01:19:59 +0000

    #safespace

---
+++ Peaches.py
@@ -0,0 +1,21 @@
+print("Hello-World")
+
+print("horses")
+
+print("cows jumping over the moon")
+
+print("icecream")
+
+print("cats, and hats")
+
+print("it's raining cats and dogs")
+      
+print("houses on chicken feet")
+
+print("cats are better then birds")
+
+print("flames and candle, wax ro") #the privious statement will still exicute
+
+# print is a function that tskes in a string and "prints" it to the console window
+
+
